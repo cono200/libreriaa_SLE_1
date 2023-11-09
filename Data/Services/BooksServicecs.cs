75595b072dde86cd62a46b6cb839eef13efe313a
cono200 <tatattian04@gmail.com>
2023-11-08 23:28:58 -0700

    Version que tiene el tercer video de Obtener los autores de un libro con HttpGet

--- Data/Services/BooksServicecs.cs
+++ Data/Services/BooksServicecs.cs
@@ -57,7 +57,24 @@ namespace libreriaa_SLE.Data.Services
 
         public List<Books> GetAllBks() => _context.Books.ToList();
         //METODO QUE NOS PERMITE OBTENER EL LIBRO QUE ESTAMOS PIDIDIENDO EN LA BASE DE DATOS
-        public Books GetBooksById(int bookid) => _context.Books.FirstOrDefault(n => n.id == bookid);
+        public BookWithAuthorsVM GetBooksById(int bookid)
+        {
+            var _bookWithAuthors = _context.Books.Where(n => n.id == bookid).Select(book => new BookWithAuthorsVM()
+            {
+                Titulo = book.Titulo,
+                Descripcion = book.Descripcion,
+                IsRead = book.IsRead,
+                DateRead = book.DateRead,
+                Rate = book.Rate,
+                Genero = book.Genero,
+                CoverUrl = book.CoverUrl,
+                PublisherName = book.Publisher.Name,
+                AutorNames = book.Book_Authors.Select(n => n.Author.FullName).ToList()
+
+            }).FirstOrDefault();
+            return _bookWithAuthors;
+            
+        }
         //METODO QUE NOS PERMITE  MODIFICAR UNA LIBRO QUE SE ENCUENTRA EN LA BASE DE DATOS
 
 
